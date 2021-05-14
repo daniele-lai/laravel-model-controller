c0c8a7175074db61be1f9c783d7de39695f211d3
Daniele <danielelai86@gmail.com>
2021-05-14 17:09:21 +0200

    Aggiunta bozza css

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,22 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
     <title>Document</title>
 </head>
 <body>
-    <h1>Quentin Tarantino Filmography</h1>
-    @foreach ($movies as $movie)
-        <ul>
-            <li>
-                <h2>{{$movie->title}}</h2>
-            </li>
-            <li>Year: {{$movie->year}}</li>
-            <li>Genre: {{$movie->genre}}</li>
-            <li>Lenght:{{$movie->lenght}}</li>
-            <li><a href="{{route('movies.show', $movie->id)}}">Details</a></li>
-        </ul>
+    <div class="container">
+        <h1>Quentin Tarantino Filmography</h1>
+        <div class="box_movies">
+            @foreach ($movies as $movie)
+            <ul>
+                <li>
+                    <h2>{{$movie->title}}</h2>
+                </li>
+                <li>Year: {{$movie->year}}</li>
+                <li>Genre: {{$movie->genre}}</li>
+                <li>Lenght:{{$movie->lenght}}</li>
+                <li><a href="{{route('movies.show', $movie->id)}}">Details</a></li>
+            </ul>
     @endforeach
+        </div>
     
+    </div>
 </body>
 </html>
 
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,11 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href={{ asset('css/app.css') }}>
     <title>Document</title>
 </head>
 <body>
-    <span><a href="{{route('movies.index')}}">Home</a></span>
-    <h1>{{$movies->title}}</h1>
-    <p>{{$movies->plot}}</p>
+    <h3><a href="{{route('movies.index')}}">Home</a></h3>
+    <div class="container_show">
+        <div class="container_details">
+            <h1>{{$movies->title}}</h1>
+            <p>{{$movies->plot}}</p>
+        </div>
+        <div class="img_details">
+            <img src={{ asset('img/pngegg.png') }} alt="Pulp Fiction dance">
+        </div>
+    </div>
+    
+    
 </body>
 </html>
